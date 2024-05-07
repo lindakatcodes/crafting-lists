02c217b6a56a36576a3a01e11a6e99b3a1120667
LindaKat <lindakt16@gmail.com>
2024-05-07 17:47:38 -0500

    adding urls and sources to script file

---
+++ src/scripts/loadItemData.ts
@@ -0,0 +1,18 @@
+const itemUrls = [
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/crafting/equipment/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/crafting/tools/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/crafting/charms/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/crafting/weapons/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/builds/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/upgrades/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/resources/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/items/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/toys/",
+  "https://www.reddit.com/r/LEGOfortnite/wiki/index/recipes/machinery/",
+];
+
+// swiftieGPT example:
+// https://github.com/datastax/SwiftieGPT/blob/main/scripts/loadDb.ts
+
+// tom scraper example:
+// https://github.com/tvanantwerp/scraper-example?tab=readme-ov-file
