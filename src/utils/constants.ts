9a42dfeff83fa3f61cd9f1f85a5a1a2f386eeda4
LindaKat <lindakt16@gmail.com>
2025-08-26 13:55:30 -0500

    made _id optional so seed script can auto generate it

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { type VectorizeDoc } from "@datastax/astra-db-ts";
 
 export interface ItemObject extends VectorizeDoc {
-  _id: string;
+  _id?: string;
   item: string;
   rarity: string;
   ingredients: Array<
